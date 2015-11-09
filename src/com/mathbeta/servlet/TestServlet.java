f0a1cbc1800ae4b8da97fa79d7c1c01abf168ab6
xiuyouxu <0463023@163.com>
2015-11-09 17:49:55 +0800

    modify test servlet

---
+++ src/com/mathbeta/servlet/TestServlet.java
@@ -0,0 +1,22 @@
+package com.mathbeta.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet{
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doPost(req, resp);
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String a = req.getParameter("a");
+		resp.getWriter().write("hello, a=" + a);
+	}
+}
